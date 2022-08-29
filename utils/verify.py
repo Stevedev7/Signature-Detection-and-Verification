fea79089efcbf506a8fe5a7f883c7040975e5527
Steve Pinto <brian.pinto@niveussolutions.com>
2022-08-29 16:37:00 +0530

    remove unwanted imports

--- utils/verify.py
+++ utils/verify.py
@@ -1,14 +1,8 @@
 import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
 import tensorflow as tf
 from tensorflow.keras.preprocessing import image
 from tensorflow.keras.applications.vgg16 import preprocess_input
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from sklearn.metrics.pairwise import cosine_similarity
-import os
-import cv2
-
 
 def load_image(image_path):
     # Return the image in the format required by VGG16 model.
